0bbaa0a81e990f65bd3459c242bf4e6477ed83ba
k98940 <pv1xxb@yandex.ru>
2024-03-27 14:07:33 +0600

    feat: retry

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -110,8 +110,10 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) => (result.status === 'fulfilled' ? result.value : null))
+  );
 }
 
 /**
@@ -132,8 +134,20 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+
+function queuPromises(promises) {
+  return new Promise((resolve) => {
+    const result = [];
+    let countPromises = promises.length;
+
+    promises.forEach((promis, i) => {
+      promis.then((res) => {
+        result[i] = res;
+        countPromises -= 1;
+        if (countPromises === 0) resolve(result.join(''));
+      });
+    });
+  });
 }
 
 module.exports = {
